4943848d92ef9efe81aeec6a164894b63d26bd02
Meatherly <meatherly@gmail.com>
2012-07-10 09:31:26 -0500

    Created new branch to play with dynamic content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 StaticPages::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "stat_pages/home"
+
+  get "stat_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
