d010a4662f4e5ef5a14d40acfe4ead21e5de14c8
Meatherly <meatherly@gmail.com>
2012-07-10 09:14:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPages::Application.config.secret_token = 'bd45ea8d56fcc36eae884a3447f2bbe17a08e9308692d91cf8e529b8f601a110832a25065a7b4c626f1aadfc68694db83d8d7bc92f57dfcdebf8b02df56b90ab'
